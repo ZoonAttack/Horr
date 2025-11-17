6d6f9bd8d97e501b45f4393b28c6d2fb2c0f2910
Ali Mohamed <ali.elsabbagh@ejust.edu.eg>
2025-11-17 20:02:55 +0200

    adding specialist DTOs

--- Entities/User/Freelancer.cs
+++ Entities/User/Freelancer.cs
@@ -16,7 +16,7 @@ namespace Entities.User
         [Key]
         [ForeignKey("User")]
         public long UserId { get; set; }
-        public virtual Entities.User.User User { get; set; }
+        public virtual User User { get; set; }
 
         [Column(TypeName = "text")]
         public string Bio { get; set; }
---
+++ ServiceContracts/DTOs/User/Specialist/SpecialistCreateDTO.cs
@@ -0,0 +1,14 @@
+﻿namespace Application.DTOs.User
+{
+    /// <summary>
+    /// Data Transfer Object for creating a new User and their initial Specialist profile.
+    /// </summary>
+    public class SpecialistCreateDTO
+    {
+        public string FullName { get; set; }
+        public string Email { get; set; }
+        public string Phone { get; set; }
+        public string Password { get; set; } // The raw password to be hashed by the service layer
+        public string Specialization { get; set; }
+    }
+}
