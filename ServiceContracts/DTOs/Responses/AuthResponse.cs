3477d1a6de27931bbb86f5f32d55b4c4240e77e2
Zoon <zoonattack18@gmail.com>
2025-12-02 13:21:34 +0200

    Added identity to entities project Added auth service interface with login and register

---
+++ ServiceContracts/DTOs/Responses/AuthResponse.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServiceContracts.DTOs.Responses
+{
+    public class AuthResponse
+    {
+        public string Id { get; set; }
+        public string Email { get; set; }
+        public string Token { get; set; }        // The JWT Access Token
+        public string RefreshToken { get; set; } // The Refresh Token
+        public DateTime ExpiresAt { get; set; }  // When the token dies
+    }
+}
