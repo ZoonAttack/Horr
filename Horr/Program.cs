f5a555093ab8cde15a0c4e11f8a175cdc21e6e0f
Zoon <zoonattack18@gmail.com>
2025-12-04 17:54:59 +0200

    added cookieconfiguration to main program.cs

--- Horr/Program.cs
+++ Horr/Program.cs
@@ -7,6 +7,11 @@ namespace Horr
             var builder = WebApplication.CreateBuilder(args);
 
             builder.Services.AddControllersWithViews();
+            builder.Services.ConfigureApplicationCookie(options =>
+            {
+                options.LoginPath = "/User/Login";
+                options.AccessDeniedPath = "/User/AccessDenied";
+            });
 
             var app = builder.Build();
             
