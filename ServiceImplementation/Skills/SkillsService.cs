462c6cbdca5b0a9e9a4b9ceb3eeaa8b2f5e977b0
Ali Mohamed <ali.elsabbagh@ejust.edu.eg>
2025-12-19 14:01:17 +0200

    Feature Added: GetAllSkills

---
+++ ServiceImplementation/Skills/SkillsService.cs
@@ -0,0 +1,23 @@
+﻿using Entities;
+using Microsoft.EntityFrameworkCore;
+using ServiceContracts.DTOs.Skill.Skill;
+using Services.Skill;
+
+namespace ServiceImplementation.Skills
+{
+    public class SkillsService : ISkillService
+    {
+        private readonly AppDbContext _db;
+        public SkillsService(AppDbContext db)
+        {
+            _db = db;
+        }
+        public async Task<IEnumerable<SkillReadDTO>> GetAllSkillsAsync()
+        {
+            return await _db.Skills
+                .AsNoTracking()
+                .Select(sk => sk.Skill_To_SkillRead())
+                .ToListAsync();
+        }
+    }
+}
